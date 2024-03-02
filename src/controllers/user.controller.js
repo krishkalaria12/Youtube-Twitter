cf5f22afb35a86e78972655679992cfaa35ffa71
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-03-02 14:23:54 +0530

    fixed import statements

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,6 @@ import {User} from "../models/user.models.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
-import { response } from "express";
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
