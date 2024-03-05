16d0e6e2df095a8eea82937558d5c8f1115cc973
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-03-05 13:16:47 +0530

    implemented controllers for publish, update, delete of videos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,11 +1,10 @@
 import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
+import {Video} from "../models/video.models.js"
 import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import {deleteOnCloudinary, uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
@@ -15,27 +14,240 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+
+    if ([title, description].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    if (!req.user?._id) {
+        throw new ApiError(400, "You are not authorized to upload a video")
+    }
+
+    const videoLocalPath = req.files?.videoFile[0]?.path;
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
+
+    if (!videoLocalPath) {
+        throw new ApiError(400, "CoverImage file is required")
+    }
+
+    if (!thumbnailLocalPath) {
+        throw new ApiError(400, "CoverImage file is required")
+    }
+
+    const videoFile = await uploadOnCloudinary(videoLocalPath, "video")
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+    if (!(videoFile && thumbnail)) {
+        throw new ApiError(500, "Error uploading video and thumbnail on cloudinary")
+    }
+
+    const createVideo = await Video.create({
+        title,
+        description,
+        duration: videoFile.duration,
+        videoFile: {
+            url: videoFile.url,
+            public_id: videoFile.public_id
+        },
+        thumbnail: {
+            url: thumbnail.url,
+            public_id: thumbnail.public_id
+        },
+        owner: req.user?._id,
+        isPublished: true,
+    });
+
+    const videoUploaded = await Video.findById(createVideo._id);
+
+    if (!videoUploaded) {
+        throw new ApiError(500, "videoUpload failed please try again !!!");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, createVideo, "Video uploaded successfully"));
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(404, "No videoId provided. Please include a valid videoId in the request parameters.")
+    }
+
+    if (!isValidObjectId(req.user?._id)) {
+        throw new ApiError(400, "Invalid userId");
+    }
+
+    const videoDetails = await Video.findById(videoId);
+
+    if (!videoDetails) {
+        throw new ApiError(404, "No Such Video Found!");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, videoDetails, "Video Details")
+    )
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
+    const { title, description } = req.body;
+    const { videoId } = req.params;
 
-})
+    // TODO: update video details like title, description, thumbnail
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId");
+    }
+
+    if (!(title && description)) {
+        throw new ApiError(400, "title and description are required");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "No video found");
+    }
+
+    if (video?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(
+            400,
+            "You can't edit this video as you are not the owner"
+        );
+    }
+
+    //deleting old thumbnail and updating with new one
+    const thumbnailToDelete = video.thumbnail.public_id;
+    const thumbnailLocalPath = req.file?.path;
+
+    if (!thumbnailLocalPath) {
+        throw new ApiError(400, "thumbnail is required");
+    }
+
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+    if (!thumbnail) {
+        throw new ApiError(400, "thumbnail not found");
+    }
+
+    const updatedVideo = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                title,
+                description,
+                thumbnail: {
+                    public_id: thumbnail.public_id,
+                    url: thumbnail.url
+                }
+            }
+        },
+        { new: true }
+    );
+
+    if (!updatedVideo) {
+        throw new ApiError(500, "Failed to update video please try again");
+    }
+
+    if (updatedVideo) {
+        await deleteOnCloudinary(thumbnailToDelete);
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, updatedVideo, "Video updated successfully"));
+});
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId");
+    }
+
+    const videoDetails = await Video.findById(videoId);
+
+    if (!videoDetails) {
+        throw new ApiError(400, "No Video Such Found");
+    }
+
+    if (videoDetails?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(
+            400,
+            "You can't delete this video as you are not the owner"
+        );
+    }
+    
+    const result = await Video.findByIdAndDelete(videoDetails?._id)
+    
+    if (!result) {
+        throw new ApiError(500, "Couldnt Delete the Video")
+    }
+    
+    if (videoDetails.videoFile.public_id && videoDetails.thumbnail.public_id) {
+        await deleteOnCloudinary(videoDetails.videoFile.public_id, "video")
+        await deleteOnCloudinary(videoDetails.thumbnail.public_id)
+    }
+
+    // Delete Video Likes and Comments
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {}, "Successfully Deleted the Video")
+    )
+
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+    const { videoId } = req.params;
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid videoId");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    if (video?.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(
+            400,
+            "You can't toogle publish status as you are not the owner"
+        );
+    }
+
+    const toggledVideoPublish = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                isPublished: !video?.isPublished
+            }
+        },
+        { new: true }
+    );
+
+    if (!toggledVideoPublish) {
+        throw new ApiError(500, "Failed to toogle video publish status");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { isPublished: toggledVideoPublish.isPublished },
+                "Video publish toggled successfully"
+            )
+        );
+});
 
 export {
     getAllVideos,
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,45 +1,51 @@
-import mongoose, { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
     {
         videoFile: {
-            type: String, // cloudinary url
+            type: {
+                url: String,
+                public_id: String,
+            },
             required: true,
         },
         thumbnail: {
-            type: String, // cloudinary url
+            type: {
+                url: String,
+                public_id: String,
+            },
             required: true,
-        },
-        title: {
-            type: String, 
-            required: true,
-            trim: true
-        },
-        description: {
-            type: String, 
-            required: true,
-            trim: true,
-        },
-        duration: {
-            type: Number, // cloudinary provides it
-            required: true,
-        },
-        views: {
-            type: Number,
-            default: 0, 
-        },
-        isPublished: {
-            type: Boolean,
-            default: true,
         },
         owner: {
             type: Schema.Types.ObjectId,
             ref: "User",
-        }
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            defaultValue: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            defaultValue: false,
+        },
     },
-{timestamps: true});
+    {
+        timestamps: true,
+    }
+);
 
-videoSchema.plugin(mongooseAggregatePaginate)
-
-export const Video = mongoose.model('Video', videoSchema);
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = model("Video", videoSchema);
